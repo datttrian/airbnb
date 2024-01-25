0131dd12bb84b901fa23d32dec0d21ec1e059509
Dat Tran <datttrian@gmail.com>
2024-01-25 00:50:40 +0000

    Initialize project

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by datttrian on 2024-01-24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
